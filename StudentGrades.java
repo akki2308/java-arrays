507471d28993bfdfbc139280d0b5cfa3387c4cb5
akki2308 <95994989+akki2308@users.noreply.github.com>
2025-03-09 17:44:38 +0530

    Update StudentGrades.java

--- StudentGrades.java
+++ StudentGrades.java
@@ -8,51 +8,55 @@ public class StudentGrades {
         System.out.print("Enter the number of students: ");
         int numStudents = scanner.nextInt();
 
-        // Arrays to store marks, percentages, and grades
+        // Arrays to store marks, percentages, grades, and remarks
         int[][] marks = new int[numStudents][3]; // 3 subjects: Physics, Chemistry, Maths
         double[] percentage = new double[numStudents];
         String[] grade = new String[numStudents];
+        String[] remarks = new String[numStudents];
 
         // Taking input for each student
         for (int i = 0; i < numStudents; i++) {
             System.out.println("\nEnter marks for Student " + (i + 1) + ":");
-            for (int j = 0; j < 3; j++) {
-                String subject = (j == 0) ? "Physics" : (j == 1) ? "Chemistry" : "Maths";
 
-                // Taking valid marks input
-                do {
-                    System.out.print(subject + " marks: ");
-                    marks[i][j] = scanner.nextInt();
-                    if (marks[i][j] < 0) {
-                        System.out.println("Marks cannot be negative. Try again.");
-                    }
-                } while (marks[i][j] < 0);
-            }
+            System.out.print("Physics marks: ");
+            marks[i][0] = scanner.nextInt();
+
+            System.out.print("Chemistry marks: ");
+            marks[i][1] = scanner.nextInt();
+
+            System.out.print("Maths marks: ");
+            marks[i][2] = scanner.nextInt();
 
             // Calculating percentage
             int totalMarks = marks[i][0] + marks[i][1] + marks[i][2];
-            percentage[i] = (totalMarks / 3.0);
+            percentage[i] = totalMarks / 3.0;
 
-            // Assigning grade
-            if (percentage[i] >= 90) {
-                grade[i] = "A+";
-            } else if (percentage[i] >= 80) {
+            // Assigning grade and remarks
+            if (percentage[i] >= 80) {
                 grade[i] = "A";
+                remarks[i] = "Level 4, above agency-normalized standards";
             } else if (percentage[i] >= 70) {
                 grade[i] = "B";
+                remarks[i] = "Level 3, at agency-normalized standards";
             } else if (percentage[i] >= 60) {
                 grade[i] = "C";
+                remarks[i] = "Level 2, below, but approaching agency-normalized standards";
             } else if (percentage[i] >= 50) {
                 grade[i] = "D";
+                remarks[i] = "Level 1, well below agency-normalized standards";
+            } else if (percentage[i] >= 40) {
+                grade[i] = "E";
+                remarks[i] = "Level 1-, too below agency-normalized standards";
             } else {
-                grade[i] = "F";
+                grade[i] = "R";
+                remarks[i] = "Remedial standards";
             }
         }
 
         // Displaying results
-        System.out.println("\nStudent\tPhysics\tChemistry\tMaths\tPercentage\tGrade");
+        System.out.println("\nStudent\tPhysics\tChemistry\tMaths\tPercentage\tGrade\tRemarks");
         for (int i = 0; i < numStudents; i++) {
-            System.out.println((i + 1) + "\t" + marks[i][0] + "\t" + marks[i][1] + "\t\t" + marks[i][2] + "\t" + percentage[i] + "%\t\t" + grade[i]);
+            System.out.println((i + 1) + "\t" + marks[i][0] + "\t" + marks[i][1] + "\t\t" + marks[i][2] + "\t" + percentage[i] + "%\t\t" + grade[i] + "\t" + remarks[i]);
         }
 
         scanner.close();
